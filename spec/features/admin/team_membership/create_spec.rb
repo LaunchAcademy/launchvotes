4fa535454dafb56776815d5baf36bbcc0657605e
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-09-09 19:00:07 -0400

    Improve team membership tests

--- spec/features/admin/team_membership/create_spec.rb
+++ spec/features/admin/team_membership/create_spec.rb
@@ -2,19 +2,28 @@ require "rails_helper"
 
 feature 'admin creates a team membership', %{
 } do
-  let!(:user) { create(:user) }
   let(:admin) { create(:admin_user) }
-  let!(:team) { create(:team) }
+  let(:team) { create(:team) }
 
   scenario "signed in admin creates team membership" do
     sign_in_as(admin)
     visit admin_team_path(team)
-    select user.name, from: "User"
+    select admin.name, from: "User"
     click_button "Add To Team"
 
     expect(page).to have_content("Team Membership Created!")
     within ".team_memberships" do
-      expect(page).to have_content(user.name)
+      expect(page).to have_content(admin.name)
+    end
+  end
+
+  context "all users belong to team" do
+    let!(:team_membership) { create(:team_membership, user: admin, team: team) }
+    scenario "no form is displayed" do
+      sign_in_as(admin)
+      visit admin_team_path(team)
+
+      expect(page).to_not have_content("Add To Team")
     end
   end
 end
